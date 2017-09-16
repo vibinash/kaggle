1393027ad82557b22e70d4bf67a378389ca6ba9f
Vibinash <vibinash.thomas@gmail.com>
2017-09-16 11:12:24 -0700

    delete unused setup file

--- setup/install_jupyter_extensions.py
+++
@@ -1,9 +0,0 @@
-print 'Install Jupyter Extensions...'
-pip install jupyter_contrib_nbextensions
-
-print 'Install javascript and css files...'
-jupyter contrib nbextension install --user
-
-print 'Start Jupyter Notebook and go to HOST:8888/nbextension'
-
-
